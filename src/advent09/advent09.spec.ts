b8e20784d764a662cbfd326531957d4c22653a65
Tony McGee <tmcgee.au@gmail.com>
2018-12-10 23:34:37 +1000

    Day 9 make it fast

--- src/advent09/advent09.spec.ts
+++ src/advent09/advent09.spec.ts
@@ -5,41 +5,13 @@ import 'mocha';
 
 describe(`Advent of Code Day ${result.dayNumber}`, () => {
 
-  it('should find next marble', () => {
-    let game = result.createGame(1,1);
-    expect(game.findNextMarble([0], 0, -2)).to.equal(0);
-    expect(game.findNextMarble([0], 0, -1)).to.equal(0);
-    expect(game.findNextMarble([0], 0, 0)).to.equal(0);
-    expect(game.findNextMarble([0], 0, 1)).to.equal(0);
-    expect(game.findNextMarble([0], 0, 2)).to.equal(0);
-
-    expect(game.findNextMarble([0,1], 0, -2)).to.equal(0);
-    expect(game.findNextMarble([0,1], 0, -1)).to.equal(1);
-    expect(game.findNextMarble([0,1], 0, 0)).to.equal(0);
-    expect(game.findNextMarble([0,1], 0, 1)).to.equal(1);
-    expect(game.findNextMarble([0,1], 0, 2)).to.equal(0);
-
-    expect(game.findNextMarble([0,1], 1, -2)).to.equal(1);
-    expect(game.findNextMarble([0,1], 1, -1)).to.equal(0);
-    expect(game.findNextMarble([0,1], 1, 0)).to.equal(1);
-    expect(game.findNextMarble([0,1], 1, 1)).to.equal(0);
-    expect(game.findNextMarble([0,1], 1, 2)).to.equal(1);
-
-    expect(game.findNextMarble([0,1,2,3,4,5,6,7,8,9,10], 1, -2)).to.equal(10);
-    expect(game.findNextMarble([0,1,2,3,4,5,6,7,8,9,10], 1, -1)).to.equal(0);
-    expect(game.findNextMarble([0,1,2,3,4,5,6,7,8,9,10], 1, 0)).to.equal(1);
-    expect(game.findNextMarble([0,1,2,3,4,5,6,7,8,9,10], 1, 1)).to.equal(2);
-    expect(game.findNextMarble([0,1,2,3,4,5,6,7,8,9,10], 1, 2)).to.equal(3);
-  });
-
   it('should solve the part 1 demo', () => {
-    expect(result.createGame(9, 25).findWinner().score.toString()).to.equal('32');
-
-    expect(result.createGame(10, 1618).findWinner().score.toString()).to.equal('8317');
-    expect(result.createGame(13, 7999).findWinner().score.toString()).to.equal('146373');
-    expect(result.createGame(17, 1104).findWinner().score.toString()).to.equal('2764');
-    expect(result.createGame(21, 6111).findWinner().score.toString()).to.equal('54718');
-    expect(result.createGame(30, 5807).findWinner().score.toString()).to.equal('37305');
+    expect(result.createGame(9, 25).findWinningScore().toString()).to.equal('32');
+    expect(result.createGame(10, 1618).findWinningScore().toString()).to.equal('8317');
+    expect(result.createGame(13, 7999).findWinningScore().toString()).to.equal('146373');
+    expect(result.createGame(17, 1104).findWinningScore().toString()).to.equal('2764');
+    expect(result.createGame(21, 6111).findWinningScore().toString()).to.equal('54718');
+    expect(result.createGame(30, 5807).findWinningScore().toString()).to.equal('37305');
   });
 
   it('should solve part 1', () => {
--- src/advent09/advent09.ts
+++ src/advent09/advent09.ts
@@ -1,78 +1,50 @@
 
 import { ISolution, InputFile, Util } from '../shared';
-
-class Player {
-   id : number;
-   score : number;
-
-   constructor(id : number) {
-      this.id = id;
-      this.score = 0;
-   }
-}
+import Deque from 'double-ended-queue';
 
 class MarbleGame {
-   _players : Player[];
-   _marbles : number[];
+   _players : Deque<number>;
+   _marbles : Deque<number>;
    _poolNext : number;
    _poolMax : number;
-   _currentPlayer : number = 0;
-   _currentMarble : number = 0;
 
    constructor(playerCount : number, marbleCount : number) {
-      this._players = Util.range(playerCount, 1).map((n) => new Player(n));
-      this._marbles = [0];
+      this._players = new Deque(Util.range(playerCount).map((p) => 0));
+      this._marbles = new Deque([0]);
       this._poolNext = 1;
       this._poolMax = marbleCount;
-   }
-
-   findWinner() : Player {
       this.start();
-      return this._players.reduce((a, c) => a.score > c.score ? a : c, new Player(0));
    }
 
-   findNextMarble(arr : number[], current : number, n : number) {
-      let m = (current + n) % arr.length;
-      return m < 0 ? arr.length - Math.abs(m) : m;
+   findWinningScore() : number {
+      return this._players.toArray().reduce((a, c) => a > c ? a : c, 0);
+   }
+
+   rotateCircle(n : number, clockwise : boolean) {
+      if (clockwise) {
+         for (let i = 0; i < n; i++) { this._marbles.push(this._marbles.shift()!); }
+      } else {
+         for (let i = 0; i < n; i++) { this._marbles.unshift(this._marbles.pop()!); }
+      }
    }
 
    start() {
       while (this._poolNext <= this._poolMax) {
-         if (this._poolNext > 0 && this._poolNext % 1000 === 0) {
-            console.log(this._poolNext);
-         }
-
-         // TODO: This is unbearably slow...
-         // change _players and _marbles from array to ring buffer
-         // http://www.collectionsjs.com/deque
-
-         let player = this._players[this._currentPlayer];
+         let score = this._players.shift()!;
 
          if (this._poolNext % 23 === 0) {
-            let circleIndex = this.findNextMarble(this._marbles, this._currentMarble, -7);
-            player.score += this._poolNext;
-            player.score += this._marbles[circleIndex];
-            this._marbles.splice(circleIndex, 1);
-            this._currentMarble = circleIndex;
+            this.rotateCircle(7, false);
+            score += this._poolNext;
+            score += this._marbles.shift()!;
          } else {
-            if (this._marbles.length === 1 || this._currentMarble === this._marbles.length - 2) {
-               this._marbles.push(this._poolNext);
-               this._currentMarble = this._marbles.length - 1;
-            } else {
-               let circleIndex = this.findNextMarble(this._marbles, this._currentMarble, 2);
-               this._marbles.splice(circleIndex, 0, this._poolNext);
-               this._currentMarble = circleIndex;
-            }
+            this.rotateCircle(2, true);
+            this._marbles.unshift(this._poolNext);
          }
 
-         this._currentPlayer++;
-         if (this._currentPlayer >= this._players.length) {
-            this._currentPlayer = 0;
-         }
+         this._players.push(score);
          this._poolNext++;
 
-         //console.log(`[${player.id}][${this._currentMarble}] ${this._marbles.join(',') }`);
-         //console.log(game._players.map((p) => `Player ${p.id} = ${p.score()}`));
+         //console.log(`${this._marbles.toArray().join(',') }`);
       }
    }
 }
@@ -94,12 +66,12 @@ class Solution9 implements ISolution {
 
    solvePart1() : string {
       let [players,marbles] = this.parseInput(this._inputFileName);
-      return this.createGame(players, marbles).findWinner().score.toString();
+      return this.createGame(players, marbles).findWinningScore().toString();
    }
 
    solvePart2() : string {
       let [players,marbles] = this.parseInput(this._inputFileName);
-      return this.createGame(players, marbles*100).findWinner().score.toString();
+      return this.createGame(players, marbles*100).findWinningScore().toString();
    }
 }
 
