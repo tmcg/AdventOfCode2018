d65a8b39d91be916f92eb6c5a6e0b8c7e43e4f69
Tony McGee <tmcgee.au@gmail.com>
2018-12-11 21:25:12 +1000

    Day 11 completed

--- src/advent11/advent11.ts
+++ src/advent11/advent11.ts
@@ -1,8 +1,22 @@
 
 import { ISolution } from '../shared';
 
+class FuelCell {
+   x : number;
+   y : number;
+   power : number;
+   powerMax : number | null = null;
+   sizeMax : number | null = null;
+
+   constructor(x : number, y : number, serial : number) {
+      this.x = x;
+      this.y = y;
+      this.power = (Math.floor(((((x + 10) * y) + serial) * (x + 10)) / 100) % 10) - 5;
+   }
+}
+
 class FuelGrid {
-   _cells : number[][] = [];
+   _cells : FuelCell[][] = [];
    _maxX : number = 300;
    _maxY : number = 300;
 
@@ -10,27 +24,23 @@ class FuelGrid {
       for (let iy = 0; iy < this._maxY; iy++) {
          this._cells[iy] = [];
          for (let ix = 0; ix < this._maxX; ix++) {
-            let x = ix + 1;
-            let y = iy + 1;
-            let rackId = x + 10;
-            let power = ((rackId * y) + serial) * rackId;
-            this._cells[iy][ix] = (Math.floor(power / 100) % 10) - 5;
+            this._cells[iy][ix] = new FuelCell(ix + 1, iy + 1, serial);
          }
       }
    }
 
-   getCell(x : number, y: number) : number {
-      return this._cells[y-1][x-1];
+   getPower(x : number, y: number) : number {
+      return this._cells[y-1][x-1].power;
    }
 
-   findMax3x3() : string {
-      let max = -1000000;
+   findMaxOf3x3() : string {
+      let max : number | null = null;
       let x = 0;
       let y = 0;
       for (let iy = 0; iy < this._maxY - 2; iy++) {
          for (let ix = 0; ix < this._maxX - 2; ix++) {
-            let val = this.getValue3x3(ix, iy);
-            if (val > max) {
+            let val = this.getPowerValues(ix, iy, 3)[2];
+            if (max === null || val > max) {
                max = val;
                x = ix + 1;
                y =  iy + 1;
@@ -41,14 +51,59 @@ class FuelGrid {
       return `${x},${y}`;
    }
 
-   getValue3x3(ax : number, ay: number) {
-      let val = 0;
-      for (let iy = 0; iy < 3; iy++) {
-         for (let ix = 0; ix < 3; ix++) {
-            val += this._cells[ay+iy][ax+ix];
+   findMaxOfAny() : string {
+      this.calculatePowerValuesOfAny();
+      let cellMax : FuelCell | null = null;
+
+      for (let iy = 0; iy < this._maxY; iy++) {
+         for (let ix = 0; ix < this._maxX; ix++) {
+            let cell = this._cells[iy][ix];
+            if (cellMax === null || cell.powerMax! > cellMax.powerMax! ) {
+               cellMax = cell;
+            }
          }
       }
-      return val;
+
+      return `${cellMax!.x},${cellMax!.y},${cellMax!.sizeMax}`;
+   }
+
+   calculatePowerValuesOfAny() {
+      for (let iy = 0; iy < this._maxY; iy++) {
+         for (let ix = 0; ix < this._maxX; ix++) {
+            let powerValues : number[] = this.getPowerValues(ix, iy);
+            let powerMax : number | null = null;
+            let sizeMax : number | null = null;
+            for (let i = 0; i < powerValues.length; i++) {
+               if (powerMax === null || powerValues[i] > powerMax) {
+                  powerMax = powerValues[i];
+                  sizeMax = i + 1;
+               }
+            }
+            this._cells[iy][ix].powerMax = powerMax;
+            this._cells[iy][ix].sizeMax = sizeMax;
+         }
+      }
+   }
+
+   getPowerValues(ax : number, ay : number, size = 0) : number[] {
+      if (size > 1) {
+         // calculate sum of N x N outer edge and recursively inner squares
+         let innerValues = this.getPowerValues(ax, ay, size - 1);
+         let outerValue = 0;
+         for (let tx = ax; tx <= ax + size - 1; tx++) {
+            outerValue += this._cells[ay + size - 1][tx].power;
+         }
+         for (let ty = ay; ty <= ay + size - 2; ty++) {
+            outerValue += this._cells[ty][ax + size - 1].power;
+         }
+         innerValues.push(outerValue + innerValues[innerValues.length - 1]);
+         return innerValues;
+      } else if (size === 1) {
+         return [this._cells[ay][ax].power];
+      }
+
+      // dynamically find the max square size
+      return this.getPowerValues(ax, ay, Math.min(this._maxX - ax, this._maxY - ay));
    }
 }
 
@@ -60,11 +115,11 @@ class Solution11 implements ISolution {
    }
 
    solvePart1() : string {
-      return this.createGrid(3613).findMax3x3();
+      return this.createGrid(3613).findMaxOf3x3();
    }
 
    solvePart2() : string {
-      return 'unsolved';
+      return this.createGrid(3613).findMaxOfAny();
    }
 }
 
